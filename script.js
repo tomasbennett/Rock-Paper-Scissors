39f0ebe57dc3fbdf684100ea5814f4d227d8465e
tomasbennett <tjs.bennett@gmail.com>
2024-12-21 21:05:04 +0000

    Changing from a console rps game to display the results in the textContent of the html

--- script.js
+++ script.js
@@ -1,35 +1,62 @@
 const choices = ["rock", "paper", "scissors"];
 
+const humanTotalDisplay = document.querySelector('.human');
+const computerTotalDisplay = document.querySelector('.computer');
+const roundTotalDisplay = document.querySelector('.round-number');
+
 let roundNumber = 1;
 let humanScore = 0;
 let computerScore = 0;
+let computerChoice;
+let endCondition;
 
-function getHumanChoice(userInput) {
-    const humanChoice = prompt(userInput).toLowerCase();
+function playGame(humanChoice) {
 
-    if (choices.includes(humanChoice)) {
-        return humanChoice;
-    } else {
-        return getHumanChoice("Please input a valid choice, either rock, paper or scissors");
+    computerChoice = getComputerChoice();
+
+    playRound(humanChoice, computerChoice);
+
+    humanTotalDisplay.textContent = humanScore;
+    computerTotalDisplay.textContent = computerScore;
+
+    roundNumber++;
+
+    endCondition = humanScore >= 3 || computerScore >= 3;
+    if(endCondition){
+
+        if(humanScore > computerScore){
+            roundTotalDisplay.textContent += "\nCONGRATULATIONS!!! You beat the computer!";
+
+        } else {
+            roundTotalDisplay.textContent += "\nSorry, the computer has won this time!";
+
+        }
+
+        roundNumber = 1;
+        humanScore = 0;
+        computerScore = 0;
     }
+    
+
 }
 
-function getComputerChoice() {
-    const random = Math.floor(Math.random() * 3);
-    return choices[random];
-}
 
-const draw = "This round was a draw!";
-
+const draw = " This round was a draw!";
 function playRound(user, computer) {
 
-    console.log(`You chose ${user}, and the computer chose ${computer}:`);
+    roundTotalDisplay.textContent = `Round ${roundNumber}: You chose ${user}, and the computer chose ${computer}:`;
+
+    if(user === computer){
+        roundTotalDisplay.textContent += draw;
+    } else {
+
+    }
 
     switch (user) {
         case "rock":
             if (computer == "rock") {
 
-                console.log(draw);
+                roundTotalDisplay.textContent += draw;
 
             } else if (computer == "paper") {
 
@@ -51,7 +78,7 @@ function playRound(user, computer) {
                 
             } else if (computer == "paper") {
 
-                console.log(draw);
+                roundTotalDisplay.textContent += draw;
 
             } else {
 
@@ -73,7 +100,7 @@ function playRound(user, computer) {
 
             } else {
 
-                console.log(draw);
+                roundTotalDisplay.textContent += draw;
 
             }
         
@@ -81,32 +108,8 @@ function playRound(user, computer) {
     }
 }
 
-function playGame() {
 
-    let humanChoice;
-    let computerChoice;
-
-    while (humanScore < 3 && computerScore < 3) {
-        humanChoice = getHumanChoice("Please input your choice, either rock, paper or scissors");
-        computerChoice = getComputerChoice();
-
-        console.log(`Round ${roundNumber}:`);
-
-        playRound(humanChoice, computerChoice);
-
-        console.log(`Computer: ${computerScore}
-You: ${humanScore}`);
-
-        roundNumber++;
-    }
-
-    if (humanScore > computerScore) {
-        console.log("CONGRATULATIONS!!! You beat the computer!");
-    } else {
-        console.log("Sorry, the computer has won this time!");
-    }
-
-    roundNumber = 1;
-    humanScore = 0;
-    computerScore = 0;
+function getComputerChoice() {
+    const random = Math.floor(Math.random() * 3);
+    return choices[random];
 }
